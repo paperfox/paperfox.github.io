b704d397f6f082bded14b0d045e081390d900c5b
paperfox <ndg.paperfox@gmail.com>
2023-03-11 19:43:10 -0500

    tarot
    add in randomized images

--- tarot/src/card-shuffle.js
+++ tarot/src/card-shuffle.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import Button from 'react-bootstrap/Button';
 import logo from './logo.svg';
 import CardPlaceholder from './card-placeholder';
+import CardModal from './card-modal';
 
 // so what I need this to do is on each click generate a random number in the range an populate it 
 // in a spot so that I can use that number to serve up an image and corresponding desc
@@ -11,6 +12,7 @@ class MakeNumbers extends Component {
   constructor(){
     super();
     this.state = {data: '0'};
+    this.state = {dataImg: '0'};
   }
 
   btnClick(){
@@ -24,10 +26,12 @@ class MakeNumbers extends Component {
     const rndInt = randomIntFromInterval(1, 5);
     const rndBinary = randomBinary(1, 2);
     this.setState({data: rndInt + '_' + rndBinary});
+    this.setState({dataImg: rndInt});
   }
 
   btnClickReset(){
     this.setState({data: '0'});
+    this.setState({dataImg: '0'});
   }
 
   render(){
@@ -39,9 +43,16 @@ class MakeNumbers extends Component {
           <Button variant="secondary" onClick={this.btnClickReset.bind(this)}>Reset</Button>
         </div>
         <div className="row justify-content-center">
-          <div className="col-lg-3 col-sm-4 col-5">
+          <div className="col-lg-3 col-sm-4 col-5" id={'tarot-content-' + this.state.data}>
             <p>{this.state.data}</p>
-            <CardPlaceholder />
+            <div className="card text-white bg-dark mb-4">
+            <img src={'images/cards/' + this.state.dataImg + '.png'} className={'card-img-top tarot-' + this.state.data} alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">Title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <CardModal />
+            </div>
+          </div>
           </div>
           <div className="col-lg-3 col-sm-4 col-5">
             <p>{this.state.data}</p>
