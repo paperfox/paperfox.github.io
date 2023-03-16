d2d6371a514ca2060e0764f8e3b36e9783c0884c
paperfox <ndg.paperfox@gmail.com>
2023-03-15 20:40:53 -0400

    tarot
    set max of 3 cards

--- tarot/src/CardShuffle.js
+++ tarot/src/CardShuffle.js
@@ -5,7 +5,6 @@ import TarotCard from './Card';
 // To do list:
 // make art
 // write text
-// remove duplicate card draws
 // clean up code - 
 // rewrite array - remove 0 and 80 from deck, rewrite in json
 // make it pretty
@@ -21,25 +20,25 @@ function MakeNumbers({ tarotContent }) {
     let randomInt = Math.floor(Math.random() * (max - min + 1) + min);
     let randomBinary = Math.floor(Math.random() * (2 - 1 + 1) + 1);
 
-    setCreateCard([...createCard, 
-      {
-        id: randomInt,
-        reverseCard: '_' + randomBinary,
-        tarotText: deck[randomInt]
-      }
-    ]);    
-
-    
-    deck.splice(randomInt, 1)
-    console.log(deck);
+    if (deck.length > 77) {
+      setCreateCard([...createCard, 
+        {
+          id: randomInt,
+          reverseCard: '_' + randomBinary,
+          tarotText: deck[randomInt]
+        }
+      ]);    
+  
+      deck.splice(randomInt, 1)
+      console.log(deck);
+    }
   }
 
   const handleClickReset = () => {
     // setCreateCard([]);
     // there's got to be a better way to do this than the line below
-    { window. location. reload(false); } 
+    window.location.reload(false);
     console.log(deck);
-
   }
 
   return (
