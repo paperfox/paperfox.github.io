fa64fd264fc6198fd497611354ed66ae24663feb
paperfox <ndg.paperfox@gmail.com>
2023-04-01 15:30:50 -0400

    tarot
    add some descriptions, update images

--- tarot/src/static/Tarot-content.js
+++ tarot/src/static/Tarot-content.js
@@ -9,7 +9,7 @@ function TarotArray() {
     {
       id: 1, 
       title: 'The Fool', 
-      divMean: 'On the brink of the unknown you stand about to plunge into something new. Opportunity is hand in hand with risk, but optimism leads you to step forward without knowing what the future holds. Embrace the unlimited potential of an unwritten future and seek new experiences - trust in the journey ahead, even if you don\'t know where it may lead. Caution and mindfulness along the way are key as this card can also indicate naivety and falling into recklessness and extravagance. Take the leap of faith towards the unknown, embrace change, but don\’t lose yourself. Remember, nothing is without consequences.',
+      divMean: 'On the brink of the unknown you stand about to plunge into something new. Opportunity is hand in hand with risk, but optimism leads you to step forward without knowing what the future holds. Embrace the unlimited potential of an unwritten future and seek new experiences - trust in the journey ahead, even if you don\'t know where it may lead. Caution and mindfulness along the way are key as this card can also indicate naivety and falling into recklessness and extravagance. Take the leap of faith towards the unknown, embrace change, but don\'t lose yourself. Remember, nothing is without consequences.',
       rvsMean: 'In reverse, this card signifies blindly charging forth into the unknown, believing oneself invulnerable to risks. The fool neglects to consider dangers - to oneself or others. Overvaluing your achievements, accomplishments, or looks will lead you down a negative path.', 
       link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-fool.htm'
     },
@@ -43,38 +43,132 @@ function TarotArray() {
     {
       id: 23, 
       title: 'Ace of Staves', 
-      divMean: 'The beginning of things, the spark, the idea that sets us on a path. Whether it\'s in leisure or business, family or friendship, ideas spring from all places and can inspire in many directions.',
+      divMean: 'The spark of creativity that starts that blaze, the beginning, the start of something new. It\'s a time of bold action and you see the potential for what things may be or what may come. Trust in yourself to find success.',
       rvsMean: 'Beware excess pursuits leading to ruin. Don\'t let joy and revel cloud you to seeing the consequences of your own actions.',
       bkgInfo: 'Tarquin Merricke - Miqo\'te - Player Character, Final Fantasy XIV',
       link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#1'
     },
-    {id: 24, title: 'Two of Staves', divMean: 'This is some sample text for Staves (wands)', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#2'},
-    {id: 25, title: 'Three of Staves', divMean: 'This is some sample text for Staves (wands)', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#3'},
-    {id: 26, title: 'Four of Staves', divMean: 'This is some sample text for Staves (wands)', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#4'},
-    {id: 27, title: 'Five of Staves', divMean: 'This is some sample text for Staves (wands)', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#5'},
-    {id: 28, title: 'Six of Staves', divMean: 'This is some sample text for Staves (wands)', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#6'},
+    {
+      id: 24, 
+      title: 'Two of Staves', 
+      divMean: 'Decisions have been made and the first steps towards achieving your goals are being taken, expanding your horizons. You have a clear vision and are ready to take action to bring your ideas to life. This card is a reminder to be confident and to trust in your ability to succeed. Explore the world, it has much to offer - you may have already conquered more of it than you think.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#2'
+    },
+    {
+      id: 25, 
+      title: 'Three of Staves',
+      divMean: 'Progress, expansion, travel, and collaboration - go forth into the future with conviction. Your hard work and efforts are beginning to pay off, and you may see the results of your endeavors. Consider networking with others and seek new opportunities for growth and success. Have faith in your vision and the stable foundation you have laid. Continue to think big and keep moving forward with confidence.',
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#3'
+    },
+    {
+      id: 26, title: 'Four of Staves', 
+      divMean: 'You may be experiencing a time of joy and abundance in your life, perhaps related to a milestone, achievement, or return of some nature. Enjoy this moment of stability and harmony. Take time to appreciate the blessings in your life, connect with loved ones, and create a sense of home and security.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#4'
+    },
+    {
+      id: 27, 
+      title: 'Five of Staves', 
+      divMean: 'Competition, conflict, and struggle. Obstacles or opposition from others could be on the way, and you may need to assert yourself and defend your position or ideas. Conflict can be constructive or costly, depending on whether those involved can find common ground.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#5'
+    },
+    {
+      id: 28, 
+      title: 'Six of Staves', 
+      divMean: 'It is a time of victory, recognition, and success. You may be achieving a significant goal or receiving praise for your accomplishments. Remember to stay humble, show gratitude, and share your success with others.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#6'
+    },
     {
       id: 29, 
       title: 'Seven of Staves', 
-      divMean: 'Victory over adversity, adversaries, and in negotiations. Courage. It might look like they\'ve gotten the last laugh, but heck I\'m pole dancing on this pyre and nothing will take me down.', 
+      divMean: 'It is a struggle, but with courage you will persevere through adversity. You may be facing opposition or a difficult situation, but you have the inner strength and determination to overcome it. Stand up for yourself, be assertive, and trust in your abilities to succeed.', 
       rvsMean: 'Anxiety, enarassment and confusion. Beware of getting paralyzed by indecision.',
       bkgInfo: 'Mike Hawke - Hrothgar - Player Character, Final Fantasy XIV',
       link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#7'
     },
-    {id: 30, title: 'Eight of Staves', divMean: 'This is some sample text for Staves (wands)', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#8'},
-    {id: 31, title: 'Nine of Staves', divMean: 'This is some sample text for Staves (wands)', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#9'},
-    {id: 32, title: 'Ten of Staves', divMean: 'This is some sample text for Staves (wands)', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#10'},
-    {id: 33, title: 'Page of Staves', divMean: 'This is some sample text for Staves (wands)', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#11'},
-    {id: 34, title: 'Knight of Staves', divMean: 'This is some sample text for Staves (wands)', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#12'},
-    {id: 35, title: 'Queen of Staves', divMean: 'This is some sample text for Staves (wands)', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#13'},
-    {id: 36, title: 'King of Staves', divMean: 'This is some sample text for Staves (wands)', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#14'},
-    {id: 37, title: 'Ace of Cups', divMean: 'This is some sample text for Cups', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#1'},
-    {id: 38, title: 'Two of Cups', divMean: 'This is some sample text for Cups', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#2'},
-    {id: 39, title: 'Three of Cups', divMean: 'This is some sample text for Cups', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#3'},
-    {id: 40, title: 'Four of Cups', divMean: 'This is some sample text for Cups', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#4'},
-    {id: 41, title: 'Five of Cups', divMean: 'This is some sample text for Cups', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#5'},
-    {id: 42, title: 'Six of Cups', divMean: 'This is some sample text for Cups', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#6'},
-    {id: 43, title: 'Seven of Cups', divMean: 'This is some sample text for Cups', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#7'},
+    {
+      id: 30, 
+      title: 'Eight of Staves', 
+      divMean: 'This is a period of swift movement, obstacles, or growth, and you need to be flexible and adaptable to keep up. Take advantage of opportunities as they arise, don\'t let rapid change become a deterrent, and stay focused on your goals.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#8'
+    },
+    {
+      id: 31, 
+      title: 'Nine of Staves', 
+      divMean: 'Despite feeling weary or battle-worn, you have the determination and  perseverance to keep going. Recovering from past disadvantages has left you resilient and with greater inner strength. Stay focused on your goals, be prepared for more challenges, but trust in your ability to overcome obstacles.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#9'
+    },
+    {
+      id: 32, 
+      title: 'Ten of Staves', 
+      divMean: 'Taking on too great a burden doesn\'t ensure reward. You are feeling overwhelmed or overburdened by your current situation or responsibilities. Take a break, delegate tasks, or seek support to lighten your load and avoid burnout.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#10'
+    },
+    {
+      id: 33, 
+      title: 'Page of Staves', 
+      divMean: 'This is a chance to enthusiastically pursue a new creative project or passion. Embrace curiosity. Be inspired and open to new experiences. Trust in your intuition and explore new opportunities. With many new ideas it may be hard to pick a direction, but stay focused and forge a path forward.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#11'
+    },
+    {
+      id: 34, 
+      title: 'Knight of Staves', 
+      divMean: 'The Knight of Wands is a tarot card that represents action, adventure, and passion. Feelings of restlessness may have you seeking new adventures or challenges. Take bold action, be spontaneous, and embrace change as you pursue your goals with energy, enthusiasm, and passion.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#12'
+    },
+    {
+      id: 35, 
+      title: 'Queen of Staves', 
+      divMean: 'Confidence, courage, and leadership shine forth from you. You feel empowered and in control of your life, and are able to inspire others with your vision and passion. Trust in your instincts, stay true to your values, and lead by example as you pursue your goals.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#13'
+    },
+    {
+      id: 36, 
+      title: 'King of Staves', 
+      divMean: 'Charismatic leadership helps you share your vision with others. You may be in a position of authority. Take a leadership role in your life. Be confident in your abilities, take bold action, and inspire others with your passion and drive.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-wands.htm#14'
+    },
+    {
+      id: 37, 
+      title: 'Ace of Cups', 
+      divMean: 'A spark of joy comes alive as you connect with others, or more deeply with yourself. This is a time of new beginnings, emotional fulfillment, and new or refreshed relationships. Deepen your connection with your emotions and intuition and seek spiritual awakening. Open your heart and embrace the love and abundance that surrounds you.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#1'
+    },
+    {
+      id: 38, 
+      title: 'Two of Cups', 
+      divMean: 'Emotional connections and partnerships, trust in the bonds that connect you to others. A new relationship may be in the works, or the deepening of an existing one. This is a harmonious time of balance, cooperation, and mutual understanding between people.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#2'
+    },
+    {
+      id: 39, 
+      title: 'Three of Cups', 
+      divMean: 'This is a time of celebration with your friends or community. Joyous gatherings, such as weddings, parties, or reunions are likely. There could also be creative collaborations on the horizon. Welcome the support and encouragement of friends and loved ones.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#3'
+    },
+    {
+      id: 40, 
+      title: 'Four of Cups', 
+      divMean: 'Introspective, contemplative, and withdrawn, you\'re in a rut. Feeling stuck, bored, and apathetic could cause you to miss opportunities by being too focused on what you don\'t have, rather than appreciating what you have.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#4'
+    },
+    {
+      id: 41, 
+      title: 'Five of Cups', 
+      divMean: 'Pessimism permeates your worldview due to recent painful emotional experiences. Loss, grief, betrayal, and disappointment may be on your mind. To move forward, you need to focus on what remains rather than dwelling on what has been lost.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#5'
+    },
+    {
+      id: 42, 
+      title: 'Six of Cups', 
+      divMean: 'Nostalgia fills you as you reminisce about the past, thinking back to childhood memories, more innocent times, and reaching out to reconnect with people. Take the time you need to find joy in simple pleasures -  approach life with a childlike sense of wonder and curiosity.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#6'
+    },
+    {
+      id: 43, 
+      title: 'Seven of Cups', 
+      divMean: 'You have options, but perhaps too many? Many good things present themselves before you - you dream of having it all. When you want everything, having anything concrete becomes a fantasy. Be wary of deception and prioritize practicality and clarity over fanciful daydreams.', 
+      link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#7'
+    },
     {id: 44, title: 'Eight of Cups', divMean: 'This is some sample text for Cups', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#8'},
     {id: 45, title: 'Nine of Cups', divMean: 'This is some sample text for Cups', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#9'},
     {id: 46, title: 'Ten of Cups', divMean: 'This is some sample text for Cups', link: 'https://www.tarotcardmeanings.net/waite-tarot-comments/waite-on-tarot-cups.htm#10'},
