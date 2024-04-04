73572d87d8b70bf13fbd1f369e2217a14c1d55f5
paperfox <ndg.paperfox@gmail.com>
2024-04-04 17:37:46 -0400

    Tarot
    additional tests

--- tarot/src/App.test.js
+++ tarot/src/App.test.js
@@ -2,19 +2,38 @@ import { render, screen, waitFor } from '@testing-library/react'
 import user from '@testing-library/user-event'
 import App from './App'
 
-test('loads and shows deck', async () => {
+test('loads and shows deck', () => {
   render(<App />);
 
-  const deck = await screen.findByLabelText('Click to draw card from Tarot Deck');
+  const deck = screen.findByLabelText('Click to draw card from Tarot Deck');
   expect(deck).toBeTruthy();
 });
 
 test('draw a card from the deck', async () => {
   render(<App />);
 
+    const button = await screen.findByLabelText('Click to draw card from Tarot Deck');
+    user.click(button);
+
+    await waitFor(() => {
+      const cardInfo = screen.getAllByAltText(/info icon/i);
+      expect(cardInfo).toBeTruthy();
+    });
+});
+
+test('draw 3 cards from the deck', async () => {
+  render(<App />);
+
   const button = await screen.findByLabelText('Click to draw card from Tarot Deck');
   user.click(button);
+  const button2 = await screen.findByLabelText('Click to draw card from Tarot Deck');
+  user.click(button2);
+  const button3 = await screen.findByLabelText('Click to draw card from Tarot Deck');
+  user.click(button3);
 
-  const cardInfo = screen.findByLabelText('More information about');
-  expect(cardInfo).toBeTruthy();
+  const spreadTitles = await screen.findAllByRole('heading', {level: 2});
+  const spreadInfoButtons = screen.getAllByAltText(/info icon/i);
+
+  expect(spreadTitles).toHaveLength(10);
+  expect(spreadInfoButtons).toHaveLength(3);
 });
