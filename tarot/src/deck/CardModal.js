1f94cb3da5feafe598965411b9168166cc7f9ca1
paperfox <ndg.paperfox@gmail.com>
2024-04-04 19:44:48 -0400

    Tarot
    clean up for testing

--- tarot/src/deck/CardModal.js
+++ tarot/src/deck/CardModal.js
@@ -11,7 +11,7 @@ function CardModal({ reverseCard, dataImg, dataContent }) {
 
   return (
     <Fragment>
-      <Button variant="tertiary" className="float-end" onClick={handleShow} aria-label={`More information about ` + dataContent.title}>
+      <Button variant="tertiary" className="float-end" onClick={handleShow} aria-label={`More information about ` + dataContent.title} data-testid="info-modal-trigger">
         <img src='/images/info-icon-by-i-con.svg' className="info-icon" alt="info icon"/>
       </Button>
 
@@ -40,11 +40,6 @@ function CardModal({ reverseCard, dataImg, dataContent }) {
             </div>
           </div>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
       </Modal>
     </Fragment>
   );
