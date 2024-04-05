e350c732c3950d98ca3681fb0a2b2dcb1dc35c6e
paperfox <ndg.paperfox@gmail.com>
2024-04-04 21:01:53 -0400

    Tarot
    add more tests

--- tarot/src/App.test.js
+++ tarot/src/App.test.js
@@ -1,5 +1,6 @@
-import { render, screen, waitFor } from '@testing-library/react'
+import { findByText, render, screen, waitFor } from '@testing-library/react'
 import user from '@testing-library/user-event'
+import '@testing-library/jest-dom'
 import App from './App'
 
 test('loads and shows deck', () => {
@@ -12,28 +13,54 @@ test('loads and shows deck', () => {
 test('draw a card from the deck', async () => {
   render(<App />);
 
-    const button = await screen.findByLabelText('Click to draw card from Tarot Deck');
-    user.click(button);
+  user.click(await screen.findByLabelText('Click to draw card from Tarot Deck'));
 
-    await waitFor(() => {
-      const cardInfo = screen.getAllByAltText(/info icon/i);
-      expect(cardInfo).toBeTruthy();
-    });
+  const cardInfo = screen.getAllByAltText(/info icon/i);
+  expect(cardInfo).toBeTruthy();
 });
 
 test('draw 3 cards from the deck', async () => {
   render(<App />);
 
-  const button = await screen.findByLabelText('Click to draw card from Tarot Deck');
-  user.click(button);
-  const button2 = await screen.findByLabelText('Click to draw card from Tarot Deck');
-  user.click(button2);
-  const button3 = await screen.findByLabelText('Click to draw card from Tarot Deck');
-  user.click(button3);
+  // draw 3 cards
+  user.click(await screen.findByLabelText('Click to draw card from Tarot Deck'));
+  user.click(await screen.findByLabelText('Click to draw card from Tarot Deck'));
+  user.click(await screen.findByLabelText('Click to draw card from Tarot Deck'));
 
-  const spreadTitles = await screen.findAllByRole('heading', {level: 2});
   const spreadInfoButtons = screen.getAllByAltText(/info icon/i);
 
-  expect(spreadTitles).toHaveLength(10);
   expect(spreadInfoButtons).toHaveLength(3);
+});
+
+test('disabled after drawing 3 cards', async () => {
+  render(<App />);
+
+  // draw 3 cards
+  user.click(await screen.findByLabelText('Click to draw card from Tarot Deck'));
+  user.click(await screen.findByLabelText('Click to draw card from Tarot Deck'));
+  user.click(await screen.findByLabelText('Click to draw card from Tarot Deck'));
+
+  const disabledDeck = await screen.findByLabelText(/Click to draw card from Tarot Deck/i);
+
+  expect(disabledDeck).toBeDisabled();
+});
+
+test('open drawn card details modal', async () => {
+  render(<App />);
+
+  user.click(await screen.findByLabelText('Click to draw card from Tarot Deck'));
+  user.click(await screen.getByTestId('info-modal-trigger'));
+
+  const closeButton = await screen.findByRole('button', {name: 'Close'});
+
+  expect(closeButton).toBeTruthy();
+});
+
+test('select and view about page', async () => {
+  render(<App />);
+
+  user.click(await screen.findByRole('tab', {name: 'About', selected: false }));
+  const aboutTabClicked = await screen.findByRole('tab', {name: 'About', selected: true });
+
+  expect(aboutTabClicked).toBeTruthy();
 });
