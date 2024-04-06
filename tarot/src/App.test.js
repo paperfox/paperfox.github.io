ac7cccb8e73db125c961292cd932bb02ff01224d
paperfox <ndg.paperfox@gmail.com>
2024-04-05 22:20:12 -0400

    Tarot
    card list tab tests, ignore index in testing

--- tarot/src/App.test.js
+++ tarot/src/App.test.js
@@ -11,6 +11,8 @@ test('loads and shows deck', () => {
   expect(deck).toBeTruthy();
 });
 
+// Deck Tab
+
 test('draw a card from the deck', async () => {
   render(<App />);
 
@@ -34,7 +36,7 @@ test('draw 3 cards from the deck', async () => {
   expect(spreadTitles).toHaveLength(3);
 });
 
-test('disabled after drawing 3 cards', async () => {
+test('deck disabled after drawing 3 cards', async () => {
   render(<App />);
 
   // draw 3 cards
@@ -71,6 +73,71 @@ test('drawn card details modal closes', async () => {
 
   expect(screen.queryByRole('dialog')).toBeNull();
 });
+
+// Card List Tab
+
+test('select tab and view full list of cards and descriptions', async () => {
+  const { container } = render(<App />);
+
+  user.click(await screen.findByRole('tab', {name: 'Card List', selected: false }));
+  const aboutTabClicked = await screen.findByRole('tab', {name: 'Card List', selected: true });
+  const cardTitles = within(container.querySelector('#paperfox--tabpane-TarotCardList')).getAllByRole('heading', { level: 3 });
+
+  expect(aboutTabClicked).toBeTruthy();
+  expect(cardTitles).toHaveLength(78);
+});
+
+test('view only completed cards and descriptions', async () => {
+  const { container } = render(<App />);
+
+  user.click(screen.getByRole('button', {name: 'Completed'}));
+  const cardTitles = within(container.querySelector('#paperfox--tabpane-TarotCardList')).getAllByRole('heading', { level: 3 });
+
+  expect(cardTitles).toHaveLength(17);
+});
+
+test('view all started cards and descriptions', async () => {
+  const { container } = render(<App />);
+
+  user.click(screen.getByRole('button', {name: 'Started'}));
+  const cardTitles = within(container.querySelector('#paperfox--tabpane-TarotCardList')).getAllByRole('heading', { level: 3 });
+
+  expect(cardTitles).toHaveLength(37);
+});
+
+test('clear card filters', async () => {
+  const { container } = render(<App />);
+
+  user.click(screen.getByRole('button', {name: 'Clear Filters'}));
+  const cardTitles = within(container.querySelector('#paperfox--tabpane-TarotCardList')).getAllByRole('heading', { level: 3 });
+
+  expect(cardTitles).toHaveLength(78);
+});
+
+test('open card image view modal', async () => {
+  render(<App />);
+
+  user.click(await screen.findByLabelText('More information about The Fool'));
+
+  const closeButton = await screen.findByRole('button', {name: 'Close'});
+
+  expect(closeButton).toBeTruthy();
+});
+
+test('drawn card image view modal closes', async () => {
+  render(<App />);
+
+  user.click(await screen.findByLabelText('More information about The Fool'));
+
+  const closeButton = screen.getByRole('button', { name: /close/i });
+  await act(async () => {
+    userEvent.click(closeButton);
+  });
+
+  expect(screen.queryByRole('dialog')).toBeNull();
+});
+
+// About Tab
 
 test('select and view about page', async () => {
   render(<App />);
